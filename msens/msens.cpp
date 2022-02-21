fcd72b3da9877a31afa34755d4e9686fa7096009
iivlx <iivlx@iivlx.com>
2022-02-21 00:14:41 -0800

    Convert tabs to spaces.

--- msens/msens.cpp
+++ msens/msens.cpp
@@ -7,23 +7,23 @@ using std::string;
 using std::wstring;
 
 void printUsage() {
-	std::cout << "msens newspeed" << std::endl;
-	std::cout << "    where newspeed is the new mouse sensitivity" << std::endl;
+  std::cout << "msens newspeed" << std::endl;
+  std::cout << "    where newspeed is the new mouse sensitivity" << std::endl;
 }
 
 void setSpeed(int newSpeed) {
-	BOOL fResult = SystemParametersInfo(SPI_SETMOUSESPEED, 0, (void*)newSpeed, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+  BOOL fResult = SystemParametersInfo(SPI_SETMOUSESPEED, 0, (void*)newSpeed, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
 }
 
 int main(int argc, char* argv[]) {
-	FreeConsole();
+  FreeConsole();
 
-	if (argc <= 1) {
-		printUsage();
-		exit(1);
+  if (argc <= 1) {
+    printUsage();
+    exit(1);
 	}
 
-	int mouseSpeed = atoi(argv[1]);
+  int mouseSpeed = atoi(argv[1]);
 	
-	return 0;
+  return 0;
 }
--- mspeed/mspeed.cpp
+++ mspeed/mspeed.cpp
@@ -3,13 +3,13 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-	BOOL fResult;
-	int mouseSpeed;
-	int newSpeed = 10;
+  BOOL fResult;
+  int mouseSpeed;
+  int newSpeed = 10;
 
-	SystemParametersInfo(SPI_GETMOUSESPEED, 0, &mouseSpeed, 0);
-	std::cout << "Current speed: " << mouseSpeed << std::endl;
-	fResult = SystemParametersInfo(SPI_SETMOUSESPEED, 0, (void *)newSpeed, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+  SystemParametersInfo(SPI_GETMOUSESPEED, 0, &mouseSpeed, 0);
+  std::cout << "Current speed: " << mouseSpeed << std::endl;
+  fResult = SystemParametersInfo(SPI_SETMOUSESPEED, 0, (void *)newSpeed, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
 
-	return 0;
+  return 0;
 }
